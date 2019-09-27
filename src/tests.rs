2aa48921132e229abe9370b11fb5e2244d2688db
Andrew Abbott <mirrexagon@mirrexagon.com>
2019-09-27 15:26:59 +1000

    Implement serialization in a roundabout way

--- src/tests.rs
+++ src/tests.rs
@@ -9,3 +9,17 @@ fn train() {
 
     println!("{}", chain.generate());
 }
+
+#[test]
+#[cfg(feature = "serialization")]
+fn serialize() {
+    let mut chain = Chain::new(2);
+
+    chain.train("Hello there! I like cheese.");
+    chain.train("Hello there! The day is very nice, like a good knive.");
+
+    let json = chain.to_json().unwrap();
+    println!("{}", json);
+
+    let loaded_chain = Chain::from_json(&json);
+}
